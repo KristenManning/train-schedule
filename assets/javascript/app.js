08687843c42dd4a4fb55a2d5dabfa61781386f19
Kristen Manning <kristenm@Kristens-MacBook-Pro.local>
2016-12-22 16:30:57 -0500

    set up basic files & created form

---
+++ assets/javascript/app.js
@@ -0,0 +1,20 @@
+
+$(document).ready(function() {
+  
+
+  $(document).on("click", ".btn-success", function() {
+
+    });
+
+  // When a gif is clicked, swap the data-src2 value with the img source
+  // src = still & src2 = animated --> src = animated & src2 = still, or vice versa 
+  $(document).on("click", "img", function() {
+
+  });
+
+  // When a new emotion is submitted, add it to the emotions array and run the display buttons function again to display it  
+  $(".btn-default").on("click", function() {
+
+  });
+  
+})       
