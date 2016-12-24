74ad71899cda2215bace9287a7b124347f843920
Kristen Manning <kristenm@Kristens-MacBook-Pro.local>
2016-12-23 23:16:31 -0500

    storing user input in variables

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,18 +2,24 @@
 $(document).ready(function() {
   
 
-  $(document).on("click", ".btn-success", function() {
+  // $(document).on("click", ".btn-success", function() {
 
-    });
+  //   });
 
   // When a gif is clicked, swap the data-src2 value with the img source
   // src = still & src2 = animated --> src = animated & src2 = still, or vice versa 
-  $(document).on("click", "img", function() {
+  // $(document).on("click", "img", function() {
 
-  });
+  // });
 
   // When a new emotion is submitted, add it to the emotions array and run the display buttons function again to display it  
   $(".btn-default").on("click", function() {
+    var train = $("#u_train").val();
+    var destination = $("#u_destination").val();
+    var first_time = $("#u_first_time").val();
+    var frequency = $("#u_frequency").val();
+   
+    return false
 
   });
   
