c03cb927dc2653c780f9f79ed35961dfacc733ac
Kristen Manning <kristenm@Kristens-MacBook-Pro.local>
2016-12-27 00:02:50 -0500

    added variables for calculating time until next train and time of next train, plus jquery to display them

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -14,12 +14,20 @@ $(document).ready(function() {
 
   // When a new emotion is submitted, add it to the emotions array and run the display buttons function again to display it  
   $(".btn-default").on("click", function() {
-    var train = $("#u_train").val();
-    var destination = $("#u_destination").val();
-    var first_time = $("#u_first_time").val();
-    var frequency = $("#u_frequency").val();
+    var now = moment().format('HH:mm');
+    var train = $("#u_train").val().trim();
+    var destination = $("#u_destination").val().trim();
+    var first_time = $("#u_first_time").val().trim();
+    var first_time_con = moment(first_time, "HH:mm").subtract(1, "years");
+    var frequency = $("#u_frequency").val().trim();
 
-    $("tbody").append('<tr><td>'+train+'</td><td>'+destination+'</td><td>'+first_time+'</td><td>'+frequency+'</td></tr>');
+    var diff_in_time = moment().diff(moment(first_time_con), "minutes");
+    var min_away = frequency - (diff_in_time % frequency);
+    var next_time = moment(now, "HH:mm").add(min_away, "minutes").format("HH:mm");
+    
+
+
+    $("tbody").append('<tr><td>'+train+'</td><td>'+destination+'</td><td>'+frequency+'</td><td>'+next_time+'</td><td>'+min_away+'</td></tr>');
 
     return false
     
